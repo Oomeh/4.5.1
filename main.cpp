0985f3c69bc3a857edd1a5f2785107f8c2ed55ce
Blake Vance <nsdnmplz@gmail.com>
2017-10-04 14:30:44 -0400

    Merge origin/master
    Conflicts:
    	README.md

--- main.cpp
+++ main.cpp
@@ -16,29 +16,29 @@
 #include <iostream>
 using namespace std;
 int main() {
-    
+    //Updated version with COMMENTS
     std::string sentence;
     
     std::getline(std::cin, sentence);
 
-    
+    //Variables both for keeping track of the counter and for figuring out where the whitespace is
     int i = 0;
     int current = 0;
     int last = 0;
+    //Loops to iterate through the sentence
     while(i < sentence.size())
     {
+        //Sets the previous tester to the same number as the new one
         last = current;
+        //Tests if the current character is a space
         if(sentence[i] == ' ')
         {
+            //Sets the current to 1
             current = 1;
-            cout<< "Current = " << current << endl;
-            cout<< "Last = " << last << endl;
         }
         else
         {
             current = 0;
-            cout<< "Current = " << current << endl;
-            cout << "Last = " << last << endl;
         }
         if(current == 1 && last == 1)
         {
@@ -46,7 +46,6 @@ int main() {
         i--;
         }
 
-        cout << sentence[i] << endl;
         i++;
 
     }
